99276eaf52c109ed2603037f12f0a6f37c10752a
hmohale <hmohale@fvc.co.za>
2025-07-06 06:06:17 +0200

    Add project files.

---
+++ EduTrack/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EduTrack
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
