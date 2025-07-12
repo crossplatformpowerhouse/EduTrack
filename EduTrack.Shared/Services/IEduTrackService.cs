426977ec1d21ad38519aa76f5f5e4d0dfcabc768
Hloriso Mohale <hmohale@fvc.co.za>
2025-07-12 15:44:50 +0200

    add Interface

---
+++ EduTrack.Shared/Services/IEduTrackService.cs
@@ -0,0 +1,24 @@
+﻿using EduTrack.Shared.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduTrack.Shared.Services
+{
+	public interface IEduTrackService
+	{
+		Task<IEnumerable<User>> GetUsersAsync();
+		Task<IEnumerable<CourseType>> GetCourseTypesAsync();
+		Task<IEnumerable<Course>> GetCoursesAsync();
+		Task<IEnumerable<Subject>> GetSubjectsAsync();
+		Task<IEnumerable<AssignmentType>> GetAssignmentTypesAsync();
+		Task<IEnumerable<ExamType>> GetExamTypesAsync();
+		Task<IEnumerable<Assignment>> GetAssignmentsAsync();
+		Task<IEnumerable<Grade>> GetGradesAsync();
+		Task<IEnumerable<Account>> GetAccountsAsync();
+		Task<IEnumerable<About>> GetAboutsAsync();
+		Task<IEnumerable<StudentDetailsDto>> GetStudentDetailsAsync(int userId);
+	}
+}
