1da314e6ed030f357e24188503604605a396d204
hmohale <hmohale@fvc.co.za>
2025-07-17 15:32:22 +0200

    add Interface methods

--- EduTrack.Shared/Services/IEduTrackService.cs
+++ EduTrack.Shared/Services/IEduTrackService.cs
@@ -9,6 +9,7 @@ namespace EduTrack.Shared.Services
 {
 	public interface IEduTrackService
 	{
+		// Read methods
 		Task<IEnumerable<User>> GetUsersAsync();
 		Task<IEnumerable<CourseType>> GetCourseTypesAsync();
 		Task<IEnumerable<Course>> GetCoursesAsync();
@@ -20,5 +21,46 @@ namespace EduTrack.Shared.Services
 		Task<IEnumerable<Account>> GetAccountsAsync();
 		Task<IEnumerable<About>> GetAboutsAsync();
 		Task<IEnumerable<StudentDetailsDto>> GetStudentDetailsAsync(int userId);
+
+		// Create methods
+		Task<int> AddUserAsync(User user);
+		Task<int> AddCourseTypeAsync(CourseType courseType);
+		Task<int> AddCourseAsync(Course course);
+		Task<int> AddSubjectAsync(Subject subject);
+		Task<int> AddAssignmentTypeAsync(AssignmentType assignmentType);
+		Task<int> AddExamTypeAsync(ExamType examType);
+		Task<int> AddAssignmentAsync(Assignment assignment);
+		Task<int> AddGradeAsync(Grade grade);
+		Task<int> AddAccountAsync(Account account);
+		Task<int> AddAboutAsync(About about);
+
+		// Update methods
+		Task UpdateUserAsync(User user);
+		Task UpdateCourseTypeAsync(CourseType courseType);
+		Task UpdateCourseAsync(Course course);
+		Task UpdateSubjectAsync(Subject subject);
+		Task UpdateAssignmentTypeAsync(AssignmentType assignmentType);
+		Task UpdateExamTypeAsync(ExamType examType);
+		Task UpdateAssignmentAsync(Assignment assignment);
+		Task UpdateGradeAsync(Grade grade);
+		Task UpdateAccountAsync(Account account);
+		Task UpdateAboutAsync(About about);
+
+		// Delete methods
+		Task DeleteUserAsync(int id);
+		Task DeleteCourseTypeAsync(int id);
+		Task DeleteCourseAsync(int id);
+		Task DeleteSubjectAsync(int id);
+		Task DeleteAssignmentTypeAsync(int id);
+		Task DeleteExamTypeAsync(int id);
+		Task DeleteAssignmentAsync(int id);
+		Task DeleteGradeAsync(int id);
+		Task DeleteAccountAsync(int id);
+		Task DeleteAboutAsync(int id);
+
+		// New Notification methods
+		Task<IEnumerable<Notification>> GetNotificationsAsync(int userId);
+		Task<int> AddNotificationAsync(Notification notification);
+		Task DeleteNotificationAsync(int id);
 	}
 }
